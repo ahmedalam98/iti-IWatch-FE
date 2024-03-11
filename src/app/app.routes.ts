8c47ecd30689265477699b626efac9634713114a
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-03-11 15:21:31 +0200

    refactor: :recycle: Move single-movie to pages + Handle spinners

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { SignInComponent } from './Pages/sign-in/sign-in.component';
 import { canActivate } from './Guards/auth-guard.guard';
 import { ErrorComponent } from './Pages/error/error.component';
 import { BookingComponent } from './Pages/booking/booking.component';
-import { SingleMovieComponent } from './Components/single-movie/single-movie.component';
+import { SingleMovieComponent } from './Pages/single-movie/single-movie.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { CheckoutComponent } from './Pages/checkout/checkout.component';
 import { ProfileComponent } from './Pages/profile/profile.component';
@@ -16,9 +16,21 @@ export const routes: Routes = [
   { path: 'movie/:movie-name', component: SingleMovieComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'profile', component: ProfileComponent, canActivate:[canActivate]},
-  { path: 'booking/:movie-name', component: BookingComponent ,canActivate:[canActivate]},
-  { path: 'checkout', component: CheckoutComponent,canActivate:[canActivate] },
-  { path: 'dashboard', component: DashboardComponent,canActivate:[canActivate] },
+  { path: 'profile', component: ProfileComponent, canActivate: [canActivate] },
+  {
+    path: 'booking/:movie-name',
+    component: BookingComponent,
+    canActivate: [canActivate],
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    canActivate: [canActivate],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [canActivate],
+  },
   { path: '**', component: ErrorComponent },
 ];
